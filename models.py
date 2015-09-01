62cb5a0914067367e919b3e45c6f1c284a93e655
J. Heasly <jheasly@earthlink.net>
2015-09-01 12:23:21 -0700

    Added memorial Mass for sbecraft.

--- models.py
+++ models.py
@@ -31,13 +31,13 @@ class baseOtherServices(models.Model):
     other_services_date_time = models.DateTimeField(blank=True, null=True)
     other_services_end_date_time = models.DateTimeField(blank=True, null=True, help_text=u'(Optional.)')
     other_services_location = models.CharField(max_length=126)
-    
+
     class Meta:
         abstract = True
         verbose_name = 'Other services'
         verbose_name_plural = 'Other services'
         managed = True
-    
+
     def __unicode__(self):
         return self.description
 
@@ -45,7 +45,7 @@ class ClassifiedRep(models.Model):
     user = models.OneToOneField(User, related_name='rg_rep')
     rg_rep_phone = models.CharField(max_length=32, blank=True)
     email = models.CharField(max_length=150, blank=True)
-    
+
     def __unicode__(self):
         return self.user.username
 
@@ -108,10 +108,10 @@ class FuneralHomeProfile(models.Model):
     zip_code = models.CharField(max_length=32, blank=True)
     phone = models.CharField(max_length=14, blank=True)
     rg_rep = models.ForeignKey(ClassifiedRep, blank=True, null=True)
-    
+
     class Meta:
         ordering = ('full_name',)
-    
+
     def __unicode__(self):
         if self.city:
             return '%s in %s' % (self.full_name, self.city)
@@ -141,21 +141,21 @@ class Death_notice(models.Model):
 #     death_notice_has_run = models.BooleanField()
     death_notice_created = models.DateTimeField(auto_now_add=True)
     status = models.CharField(max_length=4, choices=STATUS, default='drft', help_text=u'Only items with a status of \'Submitted to R-G\' will be picked up for publication in the newspaper. (If the Death Notice is a work-in-progress, use the default \'Draft\' status.)</p><p><span style="color: black; font-weight: bold;">NOTE:</span> If you make a change <i style="font-weight: bold;">after</i> a Death Notice has been submitted, you <i style="font-weight: bold;">MUST</i> contact The Register-Guard newsroom.</p>')
-    
+
     class Meta:
         verbose_name = 'Death notice'
         unique_together = ('first_name', 'last_name', 'age', 'death_date',)
         ordering = ('-death_notice_created',)
-    
+
     def __unicode__(self):
         return u'%s %s %s' % (self.first_name, self.middle_name, self.last_name)
-    
+
     def save(self):
         from_email = 'rgnews.registerguard.@gmail.com'
         to_email = DN_OBIT_EMAIL_RECIPIENTS
         message_email = 'Go to the death notice admin page for further information.'
         datatuple = None
-        
+
         if(self.pk and self.status == 'live'):
             try:
                 datatuple = (
@@ -166,19 +166,19 @@ class Death_notice(models.Model):
                 datatuple = (
                     ('Change made to *PREVIOUSLY-SUBMITTED* Death Notice by %s (<--FH NEEDS PROFILE!) to %s %s death notice' % (self.funeral_home.username, self.first_name, self.last_name), message_email, from_email, to_email),
                 )
-            
+
         elif not self.pk:
             # a new Death_notice
             message_subj = 'Death notice created by %s for %s %s' % (self.funeral_home.fh_user2.full_name, self.first_name, self.last_name)
             datatuple = (message_subj, message_email, from_email, to_email,), # <- This trailing comma's vital!
-        
+
         if datatuple:
             try:
                 send_mass_mail(datatuple)
             except SMTPDataError:
                 pass
         super(Death_notice, self).save()
-    
+
     def last_name_no_suffix(self):
         suffixes = (' jr', ' sr', ' ii', ' iii')
         shortened_name = self.last_name
@@ -188,7 +188,7 @@ class Death_notice(models.Model):
                 shortened_name = self.last_name[:offset]
                 break
         return shortened_name
-    
+
     def ready_for_print(self):
         if self.status == 'live':
             return True
@@ -196,7 +196,7 @@ class Death_notice(models.Model):
             return False
     ready_for_print.boolean = True
     ready_for_print.short_description = 'Print ready'
-    
+
     def service_date(self):
         try:
             self.service
@@ -204,7 +204,7 @@ class Death_notice(models.Model):
                 return u'%s' % date(self.service.service_date_time, "P l, N j")
         except Service.DoesNotExist:
             return u'No service scheduled.'
-    
+
     def age_unit_combo(self):
         if not self.age_unit == 1: # 1 = 'years'
             return u'%s %s' % (self.age, self.get_age_unit_display())
@@ -220,6 +220,7 @@ class Service(models.Model):
         ('The funeral service followed by the burial', 'funeral service followed by the burial',),
         ('A graveside service', 'graveside service',),
         ('A musical celebration of life', 'musical celebration of life',),
+        ('A memorial Mass', 'memorial Mass',),
         ('A memorial service', 'memorial service',),
         ('A memorial service and reception', 'memorial service and reception',),
         ('A memorial service is planned', 'memorial service is planned',),
@@ -229,20 +230,20 @@ class Service(models.Model):
         ('A visitation', 'visitation',),
         ('A visitation followed by a funeral', 'visitation followed by the funeral',),
     )
-    
+
     death_notice = models.OneToOneField(Death_notice, blank=True, null=True)
     service = models.CharField(choices=SERVICES, max_length=65)
     service_date_time = models.DateTimeField(blank=True, null=True)
     service_end_date_time = models.DateTimeField(blank=True, null=True, help_text=u'(Optional.)')
     service_location = models.CharField(max_length=75, blank=True)
     service_city = models.CharField(max_length=80, blank=True)
-    
+
     class Meta:
         ordering = ('-service_date_time',)
-    
+
     def __unicode__(self):
         return self.service
-    
+
     def full_description(self):
         if self.service_extra_info:
             return u'%s %s' % (self.service, self.service_extra_info)
@@ -274,16 +275,16 @@ class Obituary(models.Model):
         (9, '9'),
         (10, '10'),
     )
-    
+
     COST = {
         'FH':    '$25',
         'STAFF': '$50',
     }
-    
-    
+
+
     def next_available_pub_date():
         right_now = datetime.datetime.now()
-        
+
         # BEFORE 2 p.m. on a MONDAY
         if right_now.hour <= 14 and right_now.weekday() == 0:
             next_pub_date = right_now.date() + datetime.timedelta(days=3)
@@ -321,12 +322,12 @@ class Obituary(models.Model):
         elif  right_now.weekday() == 6:
             next_pub_date = right_now.date() + datetime.timedelta(days=4)
         return next_pub_date
-    
+
     def obit_file_name(instance, filename):
         (orig_name, orig_ext) = path.splitext(filename)
 #         return 'obit_images/ob.%s.%s%s' % (instance.death_notice.last_name.lower(), instance.death_notice.first_name.lower(), orig_ext)
         return 'obits/%s/%s/ob.%s.%s%s' % (datetime.date.today().year, datetime.date.today().month, instance.death_notice.last_name.lower(), instance.death_notice.first_name.lower(), orig_ext)
-    
+
     user = models.ForeignKey(User, null=True, blank=True, related_name='obit_user2')
     death_notice = models.ForeignKey(Death_notice, limit_choices_to ={'death_notice_created__gte': datetime.datetime.now() - datetime.timedelta(days=DISPLAY_DAYS_BACK) })
     prepaid_by = models.CharField(max_length=325, blank=True)
@@ -342,28 +343,28 @@ class Obituary(models.Model):
     # Survivors
     status = models.CharField(max_length=4, choices=STATUS, default='drft', help_text=u'Only items with a status of \'Submitted to R-G\' will be picked up for publication in the newspaper. (If the Obituary is a work-in-progress, use the default \'Draft\' status.)</p><p><span style="color: black; font-weight: bold;">NOTE:</span> If you make a change <i style="font-weight: bold;">after</i> an Obituary has been submitted, you <i style="font-weight: bold;">MUST</i> contact your Register-Guard classified representative.</p>')
     submitted_by = models.CharField(max_length=150, blank=True, null=True)
-    
+
     obituary_in_system = models.BooleanField(u'Obit in DT?')
     obituary_publish_date = models.DateField(default=next_available_pub_date(), help_text=u"The date to be published, subject to print deadlines. If left empty, the next available date will be used.")
     obituary_created = models.DateTimeField(auto_now_add=True)
-    
+
     flag = models.BooleanField(blank=True)
     service_insignia = models.CharField(max_length=300, blank=True, help_text=u"Enter the relevant branch or branches of service.")
-    
+
     class Meta:
         verbose_name = 'Obituary'
         verbose_name_plural = 'obituaries'
         ordering = ('-obituary_created',)
-    
+
     def __unicode__(self):
         return u'Obituary for %s %s' % (self.death_notice.first_name, self.death_notice.last_name)
-    
+
     def save(self, *args, **kwargs):
         from_email = 'obituary2.registerguard.@gmail.com'
         to_email = DN_OBIT_EMAIL_RECIPIENTS
         message_email = 'Go to the obituary admin page for further information.'
         datatuple = ()
-        
+
         '''
         Change/new e-mails for changed, saved obituaries
         '''
@@ -384,7 +385,7 @@ class Obituary(models.Model):
             imaging_email.to = to_email
             imaging_email.attach(path.split(self.photo.name)[1], self.photo.read(), 'image/jpg')
             imaging_email.send(fail_silently=False)
-        
+
         '''
         Check if obituary has 'publish_date'
         '''
@@ -397,7 +398,7 @@ class Obituary(models.Model):
                     cost = self.COST['STAFF']
                 else:
                     cost = self.COST['FH']
-                
+
                 if self.user and (self.user.username in ('lcrossley', 'weeditor',)):
                     message_subj = '[Accounting] Obituary printed for %s %s' % (self.death_notice.first_name, self.death_notice.last_name)
                     message_email = 'Add %s to the invoice of %s.' % (cost, self.death_notice.funeral_home.fh_user2.full_name)
@@ -408,7 +409,7 @@ class Obituary(models.Model):
                     message_subj = '[Accounting] Obituary printed for %s %s' % (self.death_notice.first_name, self.death_notice.last_name)
                     message_email = 'Add %s to the invoice of %s. (You can also check http://www.registerguard.com/web/news/obituaries/)' % (cost, self.death_notice.funeral_home.fh_user2.full_name)
                 datatuple = (message_subj,  message_email, from_email, to_email,), # <- This trailing comma's vital!
-        
+
         '''
         Check for attachment of photo to an existing Obituary.
         '''
@@ -429,22 +430,22 @@ class Obituary(models.Model):
                 imaging_email.to = to_email
                 imaging_email.attach(path.split(self.photo.name)[1], self.photo.read(), 'image/jpg')
                 imaging_email.send(fail_silently=False)
-                
+
                 '''
                 Check for change of status from draft to live and notifying appropriate classified representative
                 '''
         # http://stackoverflow.com/questions/1355150/django-when-saving-how-can-you-check-if-a-field-has-changed
         '''
-        Can't check state of Obituary using self.pk as the Obituary has a 
-        OneToOneField relationship to the Death Notice, so they share a pk, so 
-        we use the self.obituary_created field to see if the Obituary's in the 
+        Can't check state of Obituary using self.pk as the Obituary has a
+        OneToOneField relationship to the Death Notice, so they share a pk, so
+        we use the self.obituary_created field to see if the Obituary's in the
         database.
         '''
-        
+
         '''
-        This two-part obituary_created or self.status/self.obituary_created 
-        test is to catch Obituaries that have changed state from 'Draft' to 
-        'Submitted' from both saved-exist-in-the-database or Saved on initial 
+        This two-part obituary_created or self.status/self.obituary_created
+        test is to catch Obituaries that have changed state from 'Draft' to
+        'Submitted' from both saved-exist-in-the-database or Saved on initial
         form filling out.
         '''
         if self.obituary_created is not None or (self.status == 'live' and self.obituary_created == None):
@@ -452,17 +453,17 @@ class Obituary(models.Model):
                 orig = Obituary.objects.get(pk=self.pk)
             except Obituary.DoesNotExist:
                 orig = None
-            
+
             '''
-            This 
+            This
             ( self.status/self.obituary_created ) or ( orig.status/self.status )
-            bit is the second part of the machinery to catch the change from 
-            'Draft' to 'Submitted' Obituaries for both the 
-            saved-in-the-database and those that are 'Submitted" on initial 
+            bit is the second part of the machinery to catch the change from
+            'Draft' to 'Submitted' Obituaries for both the
+            saved-in-the-database and those that are 'Submitted" on initial
             Save.
             '''
             if  (self.status =='live' and self.obituary_created == None) or (orig.status == 'drft' and self.status == 'live'):
-                
+
                 # status of obituary based on an FH-created death notice changed by FH
                 if self.user == self.death_notice.funeral_home:
                     message_subj = 'Obituary for %s %s has been released by %s' % (self.death_notice.first_name.strip(), self.death_notice.last_name.strip(), self.death_notice.funeral_home.fh_user2.full_name)
@@ -478,23 +479,23 @@ class Obituary(models.Model):
                     message_subj = 'Obituary for %s %s has been released by %s' % (self.death_notice.first_name.strip(), self.death_notice.last_name.strip(), self.user.get_full_name())
                     class_rep_to_notify = None
                 message_email = u'* Obituary text below:\n\n %s'% self.obituary_body
-                
+
                 if self.flag:
                     message_email = '* Flag insignia requested.\n\n' +  message_email
-                
+
                 if self.service_insignia:
                     message_email = ('* Service insignia requested: %s' % self.service_insignia) + '\n\n' + message_email
-                
+
                 try:
                     class_rep = self.user.email
                 except AttributeError:
                     class_rep = None
-                
+
                 if class_rep_to_notify:
                     to_email = [class_rep_to_notify, 'john.heasly@registerguard.com',]
                 elif class_rep:
                     to_email = [class_rep, 'john.heasly@registerguard.com',]
-                    
+
                 '''
                 If there's a photo, attach it
                 '''
@@ -510,14 +511,14 @@ class Obituary(models.Model):
                     img_class_message.send(fail_silently=False)
                 else:
                     datatuple = (message_subj, message_email, from_email, to_email,), # <- This trailing comma's vital!
-        
+
         if datatuple:
             try:
                 send_mass_mail(datatuple)
             except SMTPDataError:
                 pass
         super(Obituary, self).save(*args, **kwargs)
-    
+
     def admin_thumbnail(self):
         if self.photo:
             cbim = Thumbnail(self.photo.name, 180, 180)
@@ -526,7 +527,7 @@ class Obituary(models.Model):
             return u'(No photo)'
     admin_thumbnail.short_description = u'Thumbnail'
     admin_thumbnail.allow_tags = True
-    
+
     def admin_thumbnail_two(self):
         if self.photo_two:
             cbim = Thumbnail(self.photo_two.name, 180, 180)
@@ -535,7 +536,7 @@ class Obituary(models.Model):
             return u'' # Don't want to display "(No photo)" when there's no second photo.
     admin_thumbnail_two.short_description = u'Thumbnail two'
     admin_thumbnail_two.allow_tags = True
-    
+
     ##
     ## MODEL ATTRIBUTES FOR THE OBITUARY ADMIN
     ##
@@ -545,7 +546,7 @@ class Obituary(models.Model):
         else:
             return u'(No photo)'
     display_photo_file_name.short_description = u'File path'
-    
+
     def service_date(self):
         try:
             self.death_notice.service
@@ -553,7 +554,7 @@ class Obituary(models.Model):
                 return u'%s' % date(self.death_notice.service.service_date_time, "P l, N j,")
         except Service.DoesNotExist:
             return u'No service scheduled.'
-    
+
     def ready_for_print(self):
         if self.status == 'live':
             return True
